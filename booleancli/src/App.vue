4804324ae8d945f177faa44e4e0094f6165e637d
Ele-nonsochi <elea.luzi25@gmail.com>
2021-11-10 19:24:59 +0100

    init

---
+++ booleancli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+     <Title></Title>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title></Title>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Title,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
